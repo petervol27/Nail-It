e4a6d07b3d762b77d99974af3ee533d7d330d878
peter vol <petervol35@gmail.com>
2025-02-03 08:12:23 +0200

    a

--- src/firebase.js
+++ src/firebase.js
@@ -5,19 +5,27 @@ import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase configuration
+// const firebaseConfig = {
+//   apiKey: Constants.expoConfig.extra.API_KEY || process.env.API_KEY,
+//   authDomain: Constants.expoConfig.extra.AUTH_DOMAIN || process.env.AUTH_DOMAIN,
+//   projectId: Constants.expoConfig.extra.PROJECT_ID || process.env.PROJECT_ID,
+//   storageBucket:
+//     Constants.expoConfig.extra.STORAGE_BUCKET || process.env.STORAGE_BUCKET,
+//   messagingSenderId:
+//     Constants.expoConfig.extra.MESSAGING_ID || process.env.MESSAGING_ID,
+//   appId: Constants.expoConfig.extra.APP_ID || process.env.APP_ID,
+//   measurementId:
+//     Constants.expoConfig.extra.MEASUREMENT_ID || process.env.MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.API_KEY || process.env.API_KEY,
-  authDomain: Constants.expoConfig.extra.AUTH_DOMAIN || process.env.AUTH_DOMAIN,
-  projectId: Constants.expoConfig.extra.PROJECT_ID || process.env.PROJECT_ID,
-  storageBucket:
-    Constants.expoConfig.extra.STORAGE_BUCKET || process.env.STORAGE_BUCKET,
-  messagingSenderId:
-    Constants.expoConfig.extra.MESSAGING_ID || process.env.MESSAGING_ID,
-  appId: Constants.expoConfig.extra.APP_ID || process.env.APP_ID,
-  measurementId:
-    Constants.expoConfig.extra.MEASUREMENT_ID || process.env.MEASUREMENT_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
