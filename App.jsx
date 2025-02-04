0e4b8e71e83107d21f661f6917d19dfc42abea7b
Pietro <petervol35@gmail.com>
2025-02-04 20:28:58 +0200

    android works

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <View>
+      <Text>Hello</Text>
+    </View>
   );
 };
 
